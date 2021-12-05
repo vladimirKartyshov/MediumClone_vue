46ae8ef2d15627a8b459c57450d9eb7108d3d630
vladimirKartyshov <vovan198080@inbox.ru>
2021-12-05 18:40:50 +0300

    страница регистрации

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import Register from '@/views/Register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   },
 ]
 
---
+++ src/views/Register.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class='auth-page'>
+    <div class='container page'>
+      <div class='row'>
+        <div class='col-md-6 offset-md-3 col-xs-12'>
+          <h1 class='text-xs-center'>Sign Up</h1>
+          <p class='text-xs-center'>
+            <router-link :to="{name: 'login'}">Have an account?</router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent='onSubmit'>
+            <fieldset class='form-group'>
+              <input
+                type='text'
+                placeholder='Username'
+                class='form-control form-control-lg'
+              >
+            </fieldset>
+
+            <fieldset class='form-group'>
+              <input
+                type='text'
+                placeholder='Email'
+                class='form-control form-control-lg'
+              >
+            </fieldset>
+
+            <fieldset class='form-group'>
+              <input
+                type='password'
+                placeholder='Password'
+                class='form-control form-control-lg'
+              >
+            </fieldset>
+            <button class='btn btn-lg btn-primary pull-xs-right'>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRegister',
+  methods: {
+    onSubmit() {
+      console.log('submitted form')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

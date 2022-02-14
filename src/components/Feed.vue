cfcf93d149fe438e4bb0a57176f37d3596660dae
vladimirKartyshov <vovan198080@inbox.ru>
2022-02-14 04:21:48 +0300

    limit, offset, query-stringify library

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="isLoading">Loading...</div>
-    <div v-if="error">Something bad happed</div>
+    <div v-if="error">Something bad happened</div>
 
     <div v-if="feed">
       <div
@@ -42,7 +42,7 @@
         :total='total'
         :limit='limit'
         :current-page='currentPage'
-        :url='url'
+        :url='baseUrl'
       />
     </div>
   </div>
@@ -52,6 +52,8 @@
 import {mapState} from 'vuex'
 import {actionTypes} from '../store/modules/feed'
 import AppPagination from '@/components/Pagination'
+import {limit, total} from '../helpers/vars'
+import {stringify, parseUrl} from 'query-string'
 
 export default {
   name: 'AppFeed',
@@ -66,9 +68,8 @@ export default {
   },
   data() {
     return {
-      total: 500,
-      limit: 10,
-      currentPage: 5,
+      total,
+      limit,
       url: '/'
     }
   },
@@ -77,11 +78,39 @@ export default {
       isLoading: state => state.feed.isLoading,
       feed: state => state.feed.data,//data,
       error: state => state.feed.error
-    })
+    }),
+    currentPage() {
+      return Number(this.$route.query.page || '1')
+    },
+    baseUrl() {
+      return this.$route.path
+    },
+    offset() {
+      return this.currentPage * limit - limit
+    }
+  },
+  watch: {
+    currentPage() {
+      console.log('currentPage changed')
+      this.fetchFeed()
+    }
   },
   mounted() {
     console.log('init feed')
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+    this.fetchFeed()
+  },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl)
+      const stringifiedParams = stringify({
+        limit,
+        offset: this.offset,
+        ...parsedUrl.query
+      })
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
+      console.log(apiUrlWithParams)
+      this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams})
+    }
   }
 }
 </script>
---
+++ src/helpers/vars.js
@@ -0,0 +1,2 @@
+export const limit = 10
+export const total = 500
